88b654df243e1cb7d90ca3aeefcc0ef62992b3a1
Richard Baah <richardbaah@MacBook-Air-de-Richard.local>
2025-11-08 19:16:32 -0500

    prune & predicates can be handled dynamically now

--- main.go
+++ main.go
@@ -3,37 +3,61 @@ package main
 import (
 	"fmt"
 	"os"
-
-	"github.com/segmentio/parquet-go"
+	projectoptimizer "parqlite/project-optimizer"
+	"reflect"
 )
 
-func main() {
-	f, err := os.OpenFile("tmp.parquet", os.O_RDONLY, 0644)
+func handleErr(err error) {
 	if err != nil {
 		panic(err)
 	}
-	defer f.Close()
-
-	r := parquet.NewReader(f)
-	fmt.Printf("%v\n", r.Schema().Columns())
-	var count int64
-	for {
-
-		var record = make(map[string]interface{})
-		err := r.Read(&record)
-		if err != nil {
-			break
-		}
-		for k, v := range record {
-			fmt.Printf("  %s : %v\n", k, v)
-		}
-		//counties = append(counties, v)
-		genSpace(3)
-		count++
-	}
 }
-func genSpace(n int) {
-	for i := 0; i < n; i++ {
-		println("")
-	}
+func main() {
+	f, err := os.Open("data/history.parquet")
+	handleErr(err)
+	r1 := projectoptimizer.IterRowGroupsWithPrune(f, "country", "lat", "lon", "date", "temp_mean_c_approx")
+	r2 := projectoptimizer.IterRowGroupsWithPruneFilter(f, []string{"country", "lat", "lon", "date", "temp_mean_c_approx"}, func(v reflect.Value) bool {
+		lon := v.FieldByName("Lon").Float()
+		return lon > 19
+	})
+	readRecords(r1)
+	fmt.Printf("\n\n")
+	readRecords(r2)
+
 }
+
+func readRecords(r *projectoptimizer.RecordBatch) {
+	fmt.Printf("record Schmea %v\n", r.Schema)
+	for i, col := range r.Columns {
+		recordSchema := r.Schema.Fields[i]
+		name := recordSchema.Name
+		Type := recordSchema.PqType
+		fmt.Printf("column: %s | type: %v | values : %v\n", name, Type, col)
+	}
+
+}
+
+/*
+scheam message schema {
+        optional binary date (STRING);
+        optional binary country (STRING);
+        optional binary country_alpha2 (STRING);
+        optional binary capital (STRING);
+        optional double lat;
+        optional double lon;
+        optional double temp_min_c;
+        optional double temp_max_c;
+        optional double temp_mean_c_approx;
+        optional double app_temp_min_c;
+        optional double app_temp_max_c;
+        optional double precip_mm;
+        optional double rain_mm;
+        optional double snow_mm;
+        optional double windspeed_10m_max_kmh;
+        optional double windgusts_10m_max_kmh;
+        optional double wind_dir_dom_deg;
+        optional double sunshine_duration_s;
+        optional double daylight_duration_s;
+        optional double shortwave_radiation_MJ_m2;
+
+*/
